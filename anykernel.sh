e6f39cab412a6feee971a5806c02343a1745cb06
thesting7 <jamesrajput17@gmail.com>
2025-12-02 08:52:02 +0530

    anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Hydrogen Kernel by rio113 / z3rokwq
+kernel.string=Hydrogen Kernel by rio113 / z3rokwq / Sting
 do.devicecheck=1
 do.modules=0
 do.systemless=1
